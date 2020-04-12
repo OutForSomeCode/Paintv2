ec8e3ea74361809296c8c337d1f062d7fa02b3bf
Roland <roland_zwart@outlook.com>
2020-04-12 10:35:49 +0200

    added Material-UI

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 import {Circle} from "./shapes/Circle";
+import {makeStyles} from "@material-ui/core/styles";
+import FormatAlignLeftIcon from '@material-ui/icons/FormatAlignLeft';
+import FormatAlignCenterIcon from '@material-ui/icons/FormatAlignCenter';
+import FormatAlignRightIcon from '@material-ui/icons/FormatAlignRight';
+import FormatAlignJustifyIcon from '@material-ui/icons/FormatAlignJustify';
+import FormatBoldIcon from '@material-ui/icons/FormatBold';
+import FormatItalicIcon from '@material-ui/icons/FormatItalic';
+import FormatUnderlinedIcon from '@material-ui/icons/FormatUnderlined';
+import FormatColorFillIcon from '@material-ui/icons/FormatColorFill';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+
 
 class Controls extends React.Component {
     static type: IShape = new Circle();
