756cc9c6a090d4ac732128558aa1712c80c5a849
Roland <roland_zwart@outlook.com>
2020-04-28 16:37:20 +0200

    moved save/load buttons & added styling

--- src/IO/Menu.tsx
+++ src/IO/Menu.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import {FileIO} from "./FileIO";
+import {Button} from "@material-ui/core";
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import SaveIcon from '@material-ui/icons/Save';
 
-export default function IOMenu() {
+export default function IOMenu({shapeUpdate}: {shapeUpdate: any}) {
+
+    function updateCanvas() {
+        shapeUpdate();
+    }
+
+    function save() {
+        FileIO.Save();
+        updateCanvas();
+    }
+
+    function load() {
+        FileIO.Load();
+        updateCanvas();
+    }
+
     return (
         <div>
-            <button onClick={() => FileIO.Save()}>
+            <Button size={"large"} variant={"outlined"} startIcon={<SaveIcon/>} onClick={save}>
                 Save
-            </button>
-            <button onClick={() => FileIO.Load()}>
+            </Button>
+            <Button size={"large"} variant={"outlined"} startIcon={<FileCopyIcon/>} onClick={load}>
                 Load
-            </button>
+            </Button>
         </div>
     );
 }
