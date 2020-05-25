96d24a210593be25a8ac75e07d72d8335d79828e
Edgar <Edgar@AnotherFoxGuy.com>
2020-05-25 10:40:57 +0200

    :tada: Added Group saving

--- src/visitor/SaveVisitor.ts
+++ src/visitor/SaveVisitor.ts
@@ -1,27 +1,36 @@
 import {IVisitor} from "./IVisitor";
 import {Group} from "../shapes/Group";
 import {Shape} from "../shapes/Shape";
-import {FileIO} from "../IO/FileIO";
-import {Pair} from "yaml/types";
+import {Pair, YAMLSeq} from "yaml/types";
 import {sprintf} from "sprintf-js";
 
-class SaveVisitor implements IVisitor{
-    private fileIO = FileIO.getInstance();
-    visitGroup(g: Group): void {
+class SaveVisitor implements IVisitor {
+    private currentGroup = new YAMLSeq();
 
+    visitGroup(g: Group): void {
+        const dat = g.getObjectData()
+        const myVisit = new SaveVisitor()
+        dat.items.forEach(
+            (e: Shape) => {
+                e.acceptVisitor(myVisit)
+            })
+        this.currentGroup.items.push(new Pair("Group", myVisit.buildTree()))
     }
 
     visitShape(s: Shape): void {
         const shape = s.getObjectData();
-        this.fileIO.getRoot().items.push(
-            new Pair(shape.strategy, sprintf(
-                "%f, %f, %f, %f",
-                shape.pos.x,
-                shape.pos.y,
-                shape.size.x,
-                shape.size.y
-            ))
-        );
+        let kaas = new Pair(shape.strategy, sprintf(
+            "%f, %f, %f, %f",
+            shape.pos.x,
+            shape.pos.y,
+            shape.size.x,
+            shape.size.y
+        ))
+        this.currentGroup.items.push(kaas);
+    }
+
+    buildTree() {
+        return this.currentGroup;
     }
 }
 
