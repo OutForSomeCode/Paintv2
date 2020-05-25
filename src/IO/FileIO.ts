3e84e3359ea9b917b16b76873296eb1702f892df
Edgar <Edgar@AnotherFoxGuy.com>
2020-05-25 13:13:04 +0200

    :tada: Added Group loading

--- src/IO/FileIO.ts
+++ src/IO/FileIO.ts
@@ -1,6 +1,6 @@
 import YAML from 'yaml'
 import {Items} from "../shapes/Items";
-import {Scalar, YAMLMap, YAMLSeq} from "yaml/types";
+import {Scalar, YAMLMap, YAMLSeq, Pair} from "yaml/types";
 import {Circle} from "../shapes/Circle";
 import {Square} from "../shapes/Square";
 import {Rectangle} from "../shapes/Ractangle";
@@ -10,8 +10,10 @@ import {CSSProperties} from "react";
 // @ts-ignore
 import *  as scanf from 'sscanf'
 import {SaveVisitor} from "../visitor/SaveVisitor";
+import {IShape} from "../shapes/IShape";
+import {IShapeGroup} from "../shapes/IShapeGroup";
+import {Group} from "../shapes/Group";
 
-let format = "%f, %f, %f, %f";
 
 let styling: CSSProperties = {
     stroke: "black",
@@ -25,8 +27,8 @@ class FileIO {
     private constructor() {
     }
 
-    public static getInstance() :FileIO {
-        if(!FileIO._instance){
+    public static getInstance(): FileIO {
+        if (!FileIO._instance) {
             FileIO._instance = new FileIO();
         }
         return FileIO._instance;
@@ -35,39 +37,58 @@ class FileIO {
     public Load() {
         const src = localStorage.getItem('Draw');
         if (src == null) return;
-        const doc = YAML.parseDocument(src)
-        const {contents} = doc
-        if (contents instanceof Scalar) return;
+        const doc = YAML.parse(src)
 
-        let list: any[] = [];
+        console.log(YAML.parse(src, {keepNodeTypes: false, keepCstNodes: true}))
 
-        // @ts-ignore
-        contents.items.forEach(
-            (a: YAMLMap) => {
-                const parsedData = scanf(a.items[0].value.value, format);
-                console.log(a.items[0].value)
-                let type: any;
+        for (const a of doc) {
+            if (a.Group)
+                Items.getInstance().itemArray.push(this.CreateGroup(a))
+            else
+                Items.getInstance().itemArray.push(this.CreateShape(a))
+        }
+    }
 
-                switch (a.items[0].key.toString()) {
-                    case "Circle":
-                        type = new Circle()
-                        break;
-                    case "Square":
-                        type = new Square()
-                        break;
-                    case "Rectangle":
-                        type = new Rectangle()
-                        break;
-                    case "Triangle":
-                        type = new Triangle()
-                        break;
-                    default:
-                        throw new Error(`Unknown type: ${a.items[0].key}`)
+    private CreateGroup(group: any) {
+        const list: IShapeGroup[] = [];
+        for (const a of group.Group) {
+            if (a.Group)
+                list.push(this.CreateGroup(a))
+            else
+                list.push(this.CreateShape(a))
+        }
 
-                }
-                list.push(new Shape(type, parsedData[0], parsedData[1], parsedData[2], parsedData[3], styling))
-            });
-        Items.getInstance().itemArray = list;
+        let uuids = [];
+        for (const a of list) {
+            Items.getInstance().itemArray.push(a)
+            uuids.push(a.getObjectData().id)
+        }
+        let kaas = new Group(uuids)
+        console.log(uuids)
+        return kaas
+    }
+
+    private CreateShape(shape: any) {
+        console.log(shape)
+        let parsedData;
+        let type: any;
+        if (shape.Circle) {
+            type = new Circle()
+            parsedData = scanf(shape.Circle, "%f, %f, %f, %f");
+        } else if (shape.Square) {
+            type = new Square()
+            parsedData = scanf(shape.Square, "%f, %f, %f, %f");
+        } else if (shape.Rectangle) {
+            type = new Rectangle()
+            parsedData = scanf(shape.Rectangle, "%f, %f, %f, %f");
+        } else if (shape.Triangle) {
+            type = new Triangle()
+            parsedData = scanf(shape.Triangle, "%f, %f, %f, %f");
+        } else {
+            throw new Error(`Unknown type: ${shape}`)
+        }
+
+        return new Shape(type, parsedData[0], parsedData[1], parsedData[2], parsedData[3], styling)
     }
 
     public Save() {
@@ -88,6 +109,8 @@ class FileIO {
         console.log();
         localStorage.setItem('Draw', String(doc));
     }
+
+
 }
 
 export {FileIO}
